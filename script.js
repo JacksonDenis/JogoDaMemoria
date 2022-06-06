cd84c212ae2403f7cb421939589eb2bdd4641585
Jackson Denis <jackson.denis@live.com>
2022-06-06 09:59:15 -0300

    Logica

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 const cards = document.querySelectorAll('.card');
+let hasFlipperCard = false;
+let firstCard, seconSCard
 
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+    if(!hasFlipperCard) {
+        hasFlipperCard = true;
+        firstCard = this;
+        return;
+    }
+    seconSCard = this;
+    hasFlipperCard = false;
+    checkForMath();
+}
 
+function checkForMath() {
+    if(firstCard.dataset.card === seconSCard.dataset.card) {
+        disebleCards();
+        return;
+    }
+    unflipCards()
+}
+
+function disebleCards() {
+    firstCard.removeEventListener('click', flipCard);
+    seconSCard.removeEventListener('click', flipCard);
+}
+function unflipCards() {
+    setTimeout (() =>{
+        firstCard.classList.remove('flip');
+        seconSCard.classList.remove('flip');
+
+
+    },1500);
 }
 
 cards.forEach((card) => {
     card.addEventListener ('click' , flipCard)
 })
+
